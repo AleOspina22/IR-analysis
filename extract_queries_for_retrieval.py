19f593ff8a8cf7e5fb85fe7d35c49a05b90f69d9
Ivan Sekulic <ivansekulic0@gmail.com>
2023-04-06 08:27:12 +0200

    added TopicID to runs

--- extract_queries_for_retrieval.py
+++ extract_queries_for_retrieval.py
@@ -5,13 +5,15 @@ df = pd.read_csv("data/dataframeNew.csv")
 ret = []
 for i, row in df.iterrows():
     uid = row["UserID"]
+    topic = row[f"TopicID"]
     for i in range(1, 12):
         query = row[f"Query{i}"]
         if not query or type(query) == float:
             break
-        qid = uid + '-' + str(i)
+        qid = uid + '-' + topic + '-' + str(i)
         ret.append({"qid": qid,
                     "query": query.rstrip()})
 
 clean_df = pd.DataFrame(ret)
 clean_df.to_csv("data/qid_query.csv", index=False)
+
